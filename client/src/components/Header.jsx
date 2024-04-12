eb797e70576200f585009cdc79966aca65d4e6ba
SGowripooja <69110829+SGowripooja@users.noreply.github.com>
2024-04-12 13:22:04 +0530

    Authentication-client
    Nodejs- Day -5: Authentication

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+import "./Header.css";
+
+
+function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <h1>Authentication</h1>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header
